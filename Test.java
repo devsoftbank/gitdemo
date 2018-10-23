57a3886e641c5655df474f65340ace8bb646b8bb
lilei <382147032@qq.com>
2018-10-23 10:53:10 +0800

    javatestt

---
+++ Test.java
@@ -0,0 +1,27 @@
+public class Test{
+	static float c;static int d;
+	static int e=100,f=1010;
+	static byte a = 126;
+	static float b = 111111.22554F;
+	static int g = (int)(a+b+c+d+e+f);
+	static byte aa=1,bb=2;
+	static byte by = (byte)130;
+	static char ff;
+	public static void main(String[] args){
+		byte cc = (byte)(aa+bb);
+		char ee='男';
+		System.out.println(1+'a'+"hello");
+		System.out.println("hello"+'a'+1);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		System.out.println(cc);
+		System.out.println(by);
+		System.out.println(ee);
+		System.out.println(ff);
+	}
+}
